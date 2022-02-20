c78bd8004046eb24ffc68a7fa57502c089403970
kahdichienja <agooclinton@gmail.com>
2022-02-20 10:49:09 +0300

    added google map package

--- app/src/main/java/com/example/jetmap/MainActivity.kt
+++ app/src/main/java/com/example/jetmap/MainActivity.kt
@@ -1,28 +1,67 @@
 package com.example.jetmap
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetmap.ui.theme.JetMapTheme
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.*
+
+private const val TAG = "MainActivityMap"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var isMapLoaded by remember { mutableStateOf(false) }
             JetMapTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GoogleMapView(
+                        modifier = Modifier.fillMaxSize(),
+                        onMapLoaded = {
+                            isMapLoaded = true
+                        }
+                    )
+                    
+                    if(!isMapLoaded){
+                        AnimatedVisibility(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            visible = !isMapLoaded,
+                            enter = EnterTransition.None,
+                            exit = fadeOut()
+                        ) {
+                            CircularProgressIndicator(
+                                modifier = Modifier
+                                    .background(MaterialTheme.colors.background)
+                                    .wrapContentSize()
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -30,14 +69,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GoogleMapView(modifier: Modifier, onMapLoaded: () -> Unit) {
+    val singapore = LatLng(1.35, 103.87)
+    val singapore2 = LatLng(1.40, 103.77)
+
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 11f)
+    }
+
+    var mapProperties by remember {
+        mutableStateOf(MapProperties(mapType = MapType.NORMAL))
+    }
+    var uiSettings by remember {
+        mutableStateOf(
+            MapUiSettings(compassEnabled = false)
+        )
+    }
+    var shouldAnimateZoom by remember { mutableStateOf(true) }
+    var ticker by remember { mutableStateOf(0) }
+
+    GoogleMap(
+        modifier = modifier,
+        cameraPositionState = cameraPositionState,
+        properties = mapProperties,
+        uiSettings = uiSettings,
+        onMapLoaded = onMapLoaded,
+        googleMapOptionsFactory = {
+            GoogleMapOptions().camera(
+                CameraPosition.fromLatLngZoom(
+                    singapore,
+                    11f
+                )
+            )
+        },
+        onPOIClick = {
+            Log.d(TAG, "POI clicked: ${it.name}")
+        }
+    )
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetMapTheme {
-        Greeting("Android")
+        GoogleMapView(modifier = Modifier.fillMaxSize(),  onMapLoaded = {})
     }
 }
