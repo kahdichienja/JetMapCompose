4ea522d2aaa9bee3cad2f7bae64455d0a9ae8d16
kahdichienja <agooclinton@gmail.com>
2022-02-22 15:25:56 +0300

    Google Places APi

--- app/src/main/java/com/example/jetmap/feature_google_places/data/remote/GooglePlacesApi.kt
+++ app/src/main/java/com/example/jetmap/feature_google_places/data/remote/GooglePlacesApi.kt
@@ -1,4 +1,18 @@
 package com.example.jetmap.feature_google_places.data.remote
 
+import com.example.jetmap.feature_google_places.domain.model.GooglePlacesInfo
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface GooglePlacesApi {
+    @GET("/maps/api/directions/json")
+    suspend fun getDirection(
+        @Query("origin") origin: String,
+        @Query("destination") destination: String,
+        @Query("key") key: String
+    ): GooglePlacesInfo
+
+    companion object {
+        const val BASE_URL = "https://maps.googleapis.com/"
+    }
 }
