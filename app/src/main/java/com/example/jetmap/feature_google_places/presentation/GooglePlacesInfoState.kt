880238769aa596034c723e430182b1729b682b6d
kahdichienja <agooclinton@gmail.com>
2022-02-22 16:00:18 +0300

    Google Places info state

--- app/src/main/java/com/example/jetmap/feature_google_places/presentation/GooglePlacesInfoState.kt
+++ app/src/main/java/com/example/jetmap/feature_google_places/presentation/GooglePlacesInfoState.kt
@@ -1,4 +1,5 @@
 package com.example.jetmap.feature_google_places.presentation
 
-class GooglePlacesInfoState {
-}
+import com.example.jetmap.feature_google_places.domain.model.GooglePlacesInfo
+
+data class GooglePlacesInfoState(val direction: GooglePlacesInfo?, val isLoading: Boolean = false)
